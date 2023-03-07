0be047fc3a30a8848a78eb7c3802fa17f4db46f0
Tray-PC <Tray-PC@MacBook-Air-3.local>
2023-03-07 10:11:19 +0700

    refactor: create server from bin/www
    update app.js

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const express = require("express")
 const router = require('./routes/index.js')
 
 const app = express()
-const PORT = 3000
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(router)
 
-app.listen(PORT, () => {
-    console.log("App love you", PORT)
-})
+module.exports = app
